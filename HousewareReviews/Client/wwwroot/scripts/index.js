e6a3a4c2e45e4dc27ebe688fdc8625f5c6ae4faa
YOONG WAI KIT 2102916J <2102916J@student.tp.edu.sg>
2023-12-03 21:47:02 +0800

    Fixed bugs and added comments for NavMenu.

---
+++ HousewareReviews/Client/wwwroot/scripts/index.js
@@ -0,0 +1,24 @@
+﻿// Hide Collapsable Bootstrap Navbar If Browser Width Changed From Below XL (<1200px) to Above XL (>=1200px)
+let widthXL = false;
+window.addEventListener('resize', function() {
+    const currentWidth = window.innerWidth;
+    const widthXLNow = currentWidth >= 1200;
+
+    if (!widthXL && widthXLNow) {
+        hideNavBar();
+    }
+
+    widthXL = widthXLNow;
+});
+
+// Hide Collapsable Bootstrap NavBar
+function hideNavBar() {
+    let navbar = document.querySelector('#navbarNav');
+    let bsCollapse = bootstrap.Collapse.getOrCreateInstance(navbar);
+    bsCollapse.hide();
+}
+
+// Set Focus On Search Input Field
+function SetSearchInputFocus() {
+    document.getElementById('searchinput').focus();
+}
