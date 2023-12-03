9cfbc3a221bd5697dd8a246845a6353d444b4b76
YOONG WAI KIT 2102916J <2102916J@student.tp.edu.sg>
2023-12-04 00:40:24 +0800

    Set up identity server.

--- HousewareReviews/Server/Models/ApplicationUser.cs
+++ HousewareReviews/Server/Models/ApplicationUser.cs
@@ -4,5 +4,9 @@ namespace HousewareReviews.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? NRIC { get; set; }
+        public string? ProfileImageUri { get; set; }
     }
 }
