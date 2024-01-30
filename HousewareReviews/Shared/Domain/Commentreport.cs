1661d19d2395b95b7ff77c2ef8bbe56434c6b62e
ANDRINA WEI NING MORRISON 2101485G <2101485G@student.tp.edu.sg>
2024-01-30 15:45:05 +0800

    data validation, review stars

--- HousewareReviews/Shared/Domain/Commentreport.cs
+++ HousewareReviews/Shared/Domain/Commentreport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace HousewareReviews.Shared.Domain
     public class Commentreport
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Report type must be selected.")]
         public string? Category { get; set; }
+        [StringLength(300, ErrorMessage = "Description must not exceed {1} characters.")]
         public string? Description { get; set; }
         public string? Outcome {  get; set; }
         public int? ConsumerId { get; set; }
--- HousewareReviews/Shared/Domain/Reviewreport.cs
+++ HousewareReviews/Shared/Domain/Reviewreport.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace HousewareReviews.Shared.Domain
 {
 	public class Reviewreport
 	{
 		public int Id { get; set; }
+		[Required(ErrorMessage = "Report type must be selected.")]
 		public string? Category { get; set; }
-		public string? Description { get; set; }
+        [StringLength(300, ErrorMessage = "Description must not exceed {1} characters.")]
+        public string? Description { get; set; }
 		public string? Outcome { get; set; }
 		public int? ConsumerId { get; set; }
 		public virtual Consumer? Consumer { get; set; }
